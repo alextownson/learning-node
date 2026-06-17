f1a19f5cb12205b43f0d04021cc5eb1234dcadce
Alex Townson <alexltownson@gmail.com>
2026-06-17 09:18:08 -0400

    Add PUT, DELETE, and POST route

--- 05_library_api/routes/booksRouter.js
+++ 05_library_api/routes/booksRouter.js
@@ -2,11 +2,47 @@
 
 // takes the fastify instance and optional parameters
 async function booksRouter(fastify, _opts) {
-  // register a get route with a dynamic :id parameter
+  // register a GET route with a dynamic :id parameter
   fastify.get('/:id', async (request, reply) => {
     const { id } = request.params;
     try {
       const book = { id };
+      reply.send(book);
+    } catch (e) {
+      console.error('Error occurred:', e.message);
+      reply.send(e);
+    }
+  });
+
+  // register a PUT route
+  fastify.put('/:id', async (request, reply) => {
+    const { id } = request.params;
+    try {
+      const book = { id };
+      reply.send(book);
+    } catch (e) {
+      console.error('Error occurred:', e.message);
+      reply.send(e);
+    }
+  });
+
+  // register a DELETE route
+  fastify.delete('/:id', async (request, reply) => {
+    const { id } = request.params;
+    try {
+      const book = { id };
+      reply.send(book);
+    } catch (e) {
+      console.error('Error occurred:', e.message);
+      reply.send(e);
+    }
+  });
+
+  // register a POST route
+  fastify.post('/', async (request, reply) => {
+    const { title, author } = request.body;
+    try {
+      const book = { title, author };
       reply.send(book);
     } catch (e) {
       console.error('Error occurred:', e.message);
